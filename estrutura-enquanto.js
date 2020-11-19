302a69ba6194c5c20c5226b33a847e6542cce9ea
guilherme-brigs <guigremio2004@gmail.com>
2020-11-19 18:37:45 -0300

    Primeiro commite do projeto visual lg

---
+++ estrutura-enquanto.js
@@ -0,0 +1,16 @@
+function acaoBotao(){
+    var nome, idade, limite, contador;
+
+    limite = prompt("Digite a quantidade de vezes que vai ser verificado a idade: ")
+    contador = 0
+    while (contador < limite) {
+      nome = prompt("Digite o nome da pessoa: ")
+      idade = prompt("Digite a idade de  " + nome)
+        if (idade > 18)
+        document.getElementById("paragrafo").innerText = nome + " você é corno!"
+        else
+    document.getElementById("paragrafo").innerText = nome + " você é corno!"    
+       contador++
+    
+    }
+}
---
+++ operadores-relacionais.js
@@ -0,0 +1,23 @@
+/*
+ nota01, nota02: real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+       nota01 := 15
+       nota02 := 10
+       
+       se(nota01 = nota02)entao
+            escreval("Nota01 é diferente")
+       senao
+            escreval("Nota01 é igual")
+            fimse
+*/
+
+var nota01, nota02;
+nota01 = 15
+nota02 = 10
+if(nota01 <= nota02)
+    alert("Nota01 é diferente")
+else
+    alert("Nota01 é igual")
+fimse
